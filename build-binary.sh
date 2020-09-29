af1911ac90616d0017527b5ad42a1036e194c76d
aviaviavi <avipress@gmail.com>
2020-09-29 13:35:13 -0700

    update scripts to use package.yaml instead of cabal to figure out versions

--- build-binary.sh
+++ build-binary.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 arch="$1"
-version=$(grep "^version" curl-runnings.cabal | sed 's/ //g'  | cut -d ':' -f 2)
+version=$(grep "^version" package.yaml | sed 's/ //g'  | cut -d ':' -f 2)
 archive="curl-runnings-$version-$arch.tar.gz"
 
 echo "Creating $archive"
