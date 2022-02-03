92932caa9840e26ba5d3d2bbfd26a1297aaeecef
Tim Dysinger <tim@scarf.sh>
2022-02-03 10:15:05 -0800

    Add a new internal module for aeson compatability

---
+++ src/Testing/CurlRunnings/Internal/Aeson.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE CPP #-}
+
+-- | Module for the purpose of Aeson compatibility
+module Testing.CurlRunnings.Internal.Aeson
+  ( module Map
+  , KeyType
+  , MapType
+  , findWithDefault
+  , fromText
+  , toText
+  ) where
+
+import Data.Text (Text)
+
+#if MIN_VERSION_aeson(2,0,0)
+
+import Data.Aeson.KeyMap as Map
+import Data.Aeson.Key (Key, fromText, toText)
+
+type MapType v = Map.KeyMap v
+type KeyType = Key
+
+#else
+
+import Data.HashMap.Strict as Map
+
+type MapType v = Map.HashMap Text v
+type KeyType = Text
+
+fromText :: Text -> Text
+fromText = id
+
+toText :: Text -> Text
+toText = id
+
+#endif
+
+findWithDefault :: a -> KeyType -> MapType a -> a
+findWithDefault def k kv = case Map.lookup k kv of
+    Just v -> v
+    _      -> def
+{-# INLINABLE findWithDefault #-}
